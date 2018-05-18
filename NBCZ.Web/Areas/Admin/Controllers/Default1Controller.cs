25d3f769e5bee3cfd4c8ca58453887a9ca9d42b2
chi8708 <983143462@qq.com>
2018-05-18 09:40:24 +0800

    test

---
+++ NBCZ.Web/Areas/Admin/Controllers/Default1Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NBCZ.Web.Areas.Admin.Controllers
+{
+    public class Default1Controller : Controller
+    {
+        //
+        // GET: /Admin/Default1/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
