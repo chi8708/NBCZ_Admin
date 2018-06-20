b70091d4e7e7a0db584fdd3817d67c6f5938e5a0
chi8708 <983143462@qq.com>
2018-06-20 17:19:55 +0800

    EasyuiTest 添加

---
+++ NBCZ.Web/Controllers/EasyuiTestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NBCZ.Web.Controllers
+{
+    public class EasyuiTestController : Controller
+    {
+        //
+        // GET: /EasyuiTest/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Demo() 
+        {
+           // return new EmptyResult();
+           return View();
+        }
+	}
+}
